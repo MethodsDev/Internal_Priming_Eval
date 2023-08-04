af6bef4af5804359d72f2084055b6882a260dc4f
Brian Haas <bhaas@broadinstitute.org>
2023-08-04 14:48:29 +0000

    add known polyA sites

--- util/read_3prime_end_tabulator.py
+++ util/read_3prime_end_tabulator.py
@@ -6,9 +6,11 @@ from collections import defaultdict
 import argparse
 import logging
 import subprocess
+import gzip
 
 
-POLYA_WIN_SIZE = 20
+A_FRAC_WIN_SIZE = 20
+KNOWN_POLYA_SEARCH_DIST = 50
 
 POLYA_MOTIF_pattern='(A[AT]TAAA)'
 
@@ -32,6 +34,10 @@ SQANTI3_POLYA_MOTIFS = [x.upper() for x in  [
 # polyA motifs likely from here: https://polyasite.unibas.ch/atlas
 
 
+DATADIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../data")
+
+
+
 def main():
 
     parser = argparse.ArgumentParser(description="examine 3' read internal priming",
@@ -53,13 +59,16 @@ def main():
 
 
     genome_seq_lengths = parse_genome_seq_lengths(f"{genome_fa_file}.fai")
+
+    known_polyA_sites = parse_known_polyA_sites()
     
 
     seq_extract_config = { "genome_fa" : genome_fa_file,
                            "min_count" : min_count,
                            "up_flank_dist" : up_flank_dist,
                            "down_flank_dist" : down_flank_dist,
-                           "genome_seq_lengths" : genome_seq_lengths
+                           "genome_seq_lengths" : genome_seq_lengths,
+                           "known_polyA_sites" : known_polyA_sites
     }
     
     
@@ -76,11 +85,25 @@ def main():
 
     prime3_end_counter = defaultdict(int)
 
-
+    # print header for tabular report
+    print("\t".join(["Chromosome",
+                     "Coordinate",
+                     "Strand",
+                     "Num_reads",
+                     "SeqRegion",
+                     "EndFracA",
+                     "PolyA_motif",
+                     "AtOrNearKnownPolyA"]))
+    
+    
     prev_chrom = None
 
     for read in samreader.fetch():
         chrom = samreader.get_reference_name(read.reference_id)
+
+        if chrom not in genome_seq_lengths:
+            continue # skip those not in our genome fasta file.
+        
         ref_start = read.reference_start
         ref_end = read.reference_end
         #read_align_rend = read.get_blocks()[-1][1]
@@ -89,33 +112,51 @@ def main():
         transcript_end = ref_start if strand == '-' else ref_end
 
         if prev_chrom is not None and prev_chrom != chrom:
-            dump_chrom_end_counts(prime3_end_counter, seq_extract_config)
+            dump_chrom_end_counts(prev_chrom, prime3_end_counter, seq_extract_config)
             prime3_end_counter.clear()
 
         prev_chrom = chrom
         token = f"{chrom}^{transcript_end}^{strand}"
         prime3_end_counter[token] += 1
 
-    dump_chrom_end_counts(prime3_end_counter, seq_extract_config) # get last one
+    dump_chrom_end_counts(prev_chrom, prime3_end_counter, seq_extract_config) # get last one
 
 
     sys.exit(0)
 
 
-def dump_chrom_end_counts(prime3_end_counter, seq_extract_config):
+def dump_chrom_end_counts(chromosome, prime3_end_counter, seq_extract_config):
+
+    polyA_sites = seq_extract_config['known_polyA_sites'][chromosome]
+    
     for token, count in prime3_end_counter.items():
         if count >= seq_extract_config['min_count']:
             chrom, coord, strand = token.split("^")
+            coord = int(coord)
+
+            # exclude earlier out-of-range polyA sites
+            while len(polyA_sites)>0 and coord - polyA_sites[0]['coord'] > KNOWN_POLYA_SEARCH_DIST:
+                polyA_sites.pop(0)
+
+            at_known_polyA_site = False
+            for polyA_site in polyA_sites:
+                if (abs(coord - polyA_site['coord']) <= KNOWN_POLYA_SEARCH_DIST and
+                    strand == polyA_site['strand']):
+                    at_known_polyA_site = True
+                    break
+                if polyA_site['coord'] - coord > KNOWN_POLYA_SEARCH_DIST:
+                    break
+                            
             seqregion, rel_end_coord = extract_flank_seq(chrom, coord, strand, seq_extract_config)
 
-            frac_A = compute_frac_A(seqregion, rel_end_coord, POLYA_WIN_SIZE)
+            frac_A = compute_frac_A(seqregion, rel_end_coord, A_FRAC_WIN_SIZE)
 
             polyA_signal = check_for_polyA_signal(seqregion)
             if polyA_signal:
                 polyA_signal_lc = polyA_signal.lower()
                 seqregion = seqregion.replace(polyA_signal, polyA_signal_lc)
             
-            print(f"{chrom}\t{coord}\t{strand}\t{count}\t{seqregion}\t{frac_A:.3f}\t{polyA_signal}")
+            print(f"{chrom}\t{coord}\t{strand}\t{count}\t{seqregion}\t{frac_A:.3f}\t{polyA_signal}\t{at_known_polyA_site}")
 
     return
 
@@ -205,7 +246,31 @@ def compute_frac_A(seqregion, end_coord, win_size):
 
     return frac_A
 
-            
+
+def parse_known_polyA_sites():
+
+    polyA_sites_file = os.path.join(DATADIR, "atlas.clusters.2.0.GRCh38.96.bed.gz")
+
+    known_polyA_sites = defaultdict(list)
+
+    with gzip.open(polyA_sites_file, "rt") as fh:
+        for line in fh:
+            vals = line.split("\t")
+            token = vals[3]
+            chrom, coord, strand = token.split(":")
+            chrom = f"chr{chrom}"
+            known_polyA_sites[chrom].append( { 'coord' : int(coord),
+                                               'strand' : strand } )
+
+
+    # ensure sorted
+    chroms = list(known_polyA_sites.keys())
+    for chrom in chroms:
+        known_polyA_sites[chrom] = sorted(known_polyA_sites[chrom], key=lambda x: x['coord'])
+
+    return known_polyA_sites
+
+        
 if __name__=='__main__':
     main()
 
